4271f941987779848e354d400708109613fe6570
Dhruvik <dhruvik@simplilearn.com>
2023-02-01 22:19:24 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/UserMsApplication.java
@@ -0,0 +1,15 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication	
+public class UserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMsApplication.class, args);
+	}
+
+}
